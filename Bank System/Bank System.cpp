f5f9192666310b38a16084eb68f5a6ae9f17aa88
Tareq Mujahed <tareqmujahed6@gmail.com>
2025-12-29 03:58:35 +0300

    Add banking system source code

---
+++ Bank System/Bank System.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include <iomanip>
+#include "clsUtil.h"
+#include "clsLoginScreen.h"
+
+
+int main()
+{
+ 
+
+    while(true)
+    {
+
+
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+
+
+    }
+
+}
+
---
+++ Bank System/Global.cpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include "clsUser.h"
+
+
+static clsUser CurrentUser = clsUser::Find("", "");
+
+
+
+
+
